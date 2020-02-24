e6420c7607cf2b4974db8d7b3564ec4f166eb7a1
Your Name <wamontgomery0068@gmail.com>
2020-02-24 14:51:04 -0700

    Imported AddTransaction Component

--- expense_tracker/src/App.js
+++ expense_tracker/src/App.js
@@ -5,6 +5,7 @@ import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
 import './App.css';
 
@@ -24,6 +25,8 @@ function App() {
         <IncomeExpenses />
 
         <TransactionList />
+
+        <AddTransaction />
 
       </div>
 
