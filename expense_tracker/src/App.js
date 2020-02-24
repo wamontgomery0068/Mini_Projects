6bea024ff1d22ffaad199b2eb6f0ec02312a20d3
Your Name <wamontgomery0068@gmail.com>
2020-02-24 13:13:47 -0700

    Imported the Balance Component

--- expense_tracker/src/App.js
+++ expense_tracker/src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
 // Imported Components
-import {Header} from './components/Header';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
 
 import './App.css';
 
 
 function App() {
+
   return (
+
     <div>
+
       <Header />
+
+      <div className = "container">
+
+        <Balance />
+        
+      </div>
+
     </div>
+
   );
+
 }
 
 export default App;
