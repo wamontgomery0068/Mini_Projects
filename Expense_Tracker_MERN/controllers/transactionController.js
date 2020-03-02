0e232ce07f90c5d742e16d8e7034baa1c099ae49
Your Name <wamontgomery0068@gmail.com>
2020-03-02 11:10:06 -0700

    GET, POST, DELETE template layed out

--- Expense_Tracker_MERN/controllers/transactionController.js
+++ Expense_Tracker_MERN/controllers/transactionController.js
@@ -1,7 +1,25 @@
+// Controller Methods
+
 // Description: Get all transactions
 // Route: GET /api/v1/transactions
 // Access: Public
 
 exports.getTransactions = (req, res, next) => {
     res.send('GET transaction');
+}
+
+// Description: Add transaction
+// Route: POST /api/v1/transactions
+// Access: Public
+
+exports.addTransaction = (req, res, next) => {
+    res.send('POST transaction');
+}
+
+// Description: Delete transaction
+// Route: DELETE /api/v1/transactions/:id
+// Access: Public
+
+exports.deleteTransactions = (req, res, next) => {
+    res.send('DELETE transaction');
 }
