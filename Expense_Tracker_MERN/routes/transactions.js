19861f1f4fe0afe33f0b54757531d2f9fcd5fd33
Your Name <wamontgomery0068@gmail.com>
2020-03-02 10:59:27 -0700

    Tested on Postman and it Works

--- Expense_Tracker_MERN/routes/transactions.js
+++ Expense_Tracker_MERN/routes/transactions.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { getTransactions } = require('../controllers/transactionController');
 
-router.get('/', (req, res) => res.send('Hello There'));
+// router.get('/', (req, res) => res.send('Hello There'));
+
+router.route('/').get(getTransactions);
 
 module.exports = router;
