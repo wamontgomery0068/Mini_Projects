c6416ffba184dcb40bb91a7320f42a67077f24da
Your Name <wamontgomery0068@gmail.com>
2020-03-02 11:50:39 -0700

    Tested on Postman

--- Expense_Tracker_MERN/routes/transactions.js
+++ Expense_Tracker_MERN/routes/transactions.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { getTransactions } = require('../controllers/transactionController');
+const { getTransactions, addTransaction, deleteTransactions } = require('../controllers/transactionController');
 
 // router.get('/', (req, res) => res.send('Hello There'));
 
-router.route('/').get(getTransactions);
+router
+    .route('/')
+    .get(getTransactions)
+    .post(addTransaction)
+;
+
+router
+    .route('/:id')
+    .delete(deleteTransactions)
+;
+
 
 module.exports = router;
