aa83f4a24e841776f20c303e37a4495bdd3a41f4
Your Name <wamontgomery0068@gmail.com>
2020-03-03 12:35:39 -0700

    Post logic inserted and tested

--- Expense_Tracker_MERN/controllers/transactionController.js
+++ Expense_Tracker_MERN/controllers/transactionController.js
@@ -1,11 +1,14 @@
 const Transaction = require('../models/Transaction');
 
 // Description: Get all transactions
-// Route: GET /api/v1/transactions
+// Route: ** GET ** /api/v1/transactions
 // Access: Public
-// Test: Use Postman to Test your "getTransaction" request
 
 exports.getTransactions = async (req, res, next) => {
+
+    // Prior to inserting the try catch I used the res.send('GET transaction') to *** Test in POSTMAN ***
+    // res.send('GET transaction');
+
     try {
         const transactions = await Transaction.find();
 
@@ -16,7 +19,7 @@ exports.getTransactions = async (req, res, next) => {
         });
     } catch (err) {
         
-        return res.send(500).json({
+        return res.status(500).json({
             success: false,
             error: 'Server Error'
         });
@@ -25,15 +28,52 @@ exports.getTransactions = async (req, res, next) => {
 }
 
 // Description: Add transaction
-// Route: POST /api/v1/transactions
+// Route: ** POST ** /api/v1/transactions
 // Access: Public
 
 exports.addTransaction = async (req, res, next) => {
-    res.send('POST transaction');
+
+    // Prior to inserting the try catch I used the res.send('POST transaction') to *** Test in POSTMAN ***
+    // res.send('POST transaction');
+
+    try {
+        
+        const { text, amount } = req.body;
+        
+        const transaction = await Transaction.create(req.body);
+        
+        return res.status(201).json({
+            success: true,
+            data: transaction
+        });
+
+    } catch (err) {
+
+        if(err.name === 'ValidationError') {
+
+            const messages = Object.values(err.errors).map(val => val.message);
+
+            return res.status(400).json({
+                success: false,
+                error: messages
+            })
+
+        } else {
+
+            return res.status(500).json({
+                success: false,
+                error: 'Server Error'
+            });
+
+        }
+
+
+    }
+
 }
 
 // Description: Delete transaction
-// Route: DELETE /api/v1/transactions/:id
+// Route: ** DELETE ** /api/v1/transactions/:id
 // Access: Public
 
 exports.deleteTransactions = async (req, res, next) => {
