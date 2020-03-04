22bd65685cdfd7b853fad46c7afff59315154cb5
Your Name <wamontgomery0068@gmail.com>
2020-03-04 08:56:55 -0700

    Changes Made

--- Expense_Tracker_MERN/client/src/context/GlobalState.js
+++ Expense_Tracker_MERN/client/src/context/GlobalState.js
@@ -48,11 +48,27 @@ export const GlobalProvider = ({ children }) => {
 
     // ** Actions **
     // These Actions are going to make calls to our Reducer
-    function deleteTransaction(id) {
-        dispatch({
-            type: 'DELETE_TRANSACTION',
-            payload: id
-        });
+    async function deleteTransaction(id) {
+
+        try {
+            
+            await axios.delete(`/api/v1/transactions/${id}`);
+
+            dispatch({
+                type: 'DELETE_TRANSACTION',
+                payload: id
+            });
+
+        } catch (err) {
+
+            
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                payload: err.response.data.error
+            });
+            
+        }
+
     }
 
     // ** Actions **
