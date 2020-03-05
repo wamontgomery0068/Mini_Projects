1865905ff2426b4bc51a58b404aeb2dbb75675cb
Your Name <wamontgomery0068@gmail.com>
2020-03-05 07:26:10 -0700

    Created a variable to contain functions

---
+++ Jest_Testing_Basics/functions.js
@@ -0,0 +1,9 @@
+// Create a variable to contain our functions
+const functions = {
+    // Arrow Function "ES6"
+    add: (num1, num2) => num1 + num2,
+    // Normal Function
+    add: function(num1, num2){
+        return num1 + num2;
+    }
+}
