d480a196d066b5a052d845911a3f6249a2b4fd6f
Your Name <wamontgomery0068@gmail.com>
2020-03-09 08:28:12 -0600

    Routes inserted

--- Auth_Login_App/app.js
+++ Auth_Login_App/app.js
@@ -2,8 +2,15 @@
 const express = require('express');
 const colors = require('colors');
 
+
+
 // Basic Express Server
 const app = express();
+
+// ** Routes **
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
 
 // Create a Port for our App to run on
 const PORT = process.env.PORT || 5000;
