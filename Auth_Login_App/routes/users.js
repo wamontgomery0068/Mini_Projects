9258520856b01208bf01ee5da04abb5528d5fcaf
Your Name <wamontgomery0068@gmail.com>
2020-03-17 08:22:06 -0600

    Register Handler added

--- Auth_Login_App/routes/users.js
+++ Auth_Login_App/routes/users.js
@@ -8,5 +8,41 @@ router.get('/login', (req, res) => res.render('login'));
 // Register Page
 router.get('/register', (req, res) => res.render('register'));
 
+// Register Handler
+router.post('/register', (req, res) => {
+
+    const { name, email, password, password2 } = req.body;
+
+    let errors = [];
+
+    // Check required fields
+    if(!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please fill in all fields'})
+    };
+
+    // Check if passwords match
+    if(password !== password2) {
+        errors.push({ msg: 'Passwords do not match'})
+    };
+
+    // Check the password length
+    if(password.length < 6) {
+        errors.push({ msg: 'Password must be greater than 6 characters'})
+    };
+
+    if(errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        })
+    } else {
+        res.send(' Passed ');
+    }
+
+});
+
 
 module.exports = router;
